e9639b24a904c4f2411bd27d87f2e961c7ee84ad
Cody Large <dlarge1@ewu.edu>
2023-11-09 15:05:01 -0800

    Styling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Document</title>
 </head>
 <body>
@@ -22,7 +23,7 @@
     </form>
 
     <form class="searchform" action="search.php" method="post">
-        <label for="search">Search for user:</label>
+        <h3 for="search">Search for user:</h3>
         <input id="search" type="text" name="usersearch" placeholder="Search...">
         <button>Search</button>
     </form>
